93d286ddeaacba8dad9d82b976839d0376b5ca26
MircoSil <mirco.sildatke@gmail.com>
2025-09-28 09:09:40 +0200

    test1.5

--- deploy/start.ps1
+++ deploy/start.ps1
@@ -9,4 +9,5 @@ if (docker ps -a --format "{{.Names}}" | Select-String -SimpleMatch $Name) {
   docker rm -f $Name | Out-Null
 }
 docker run -d --name $Name -p ${Port}:80 --restart unless-stopped $Image
-Write-Host "Running $Name on http://localhost:$Port"
+$InformationPreference = 'Continue'
+Write-Information "Running $Name on http://localhost:$Port"
--- deploy/update.ps1
+++ deploy/update.ps1
@@ -15,7 +15,10 @@ if (docker ps -a --format "{{.Names}}" | Select-String -SimpleMatch $Name) {
     docker stop $Name | Out-Null
     docker rm $Name   | Out-Null
   }
-  
+
   # Neu starten mit gewünschtem Port
   docker run -d --name $Name -p ${Port}:80 --restart unless-stopped $Image
-  Write-Host "Updated $Name to $Image on http://localhost:$Port"
+  $InformationPreference = 'Continue'   # damit wird Information ausgegeben
+  Write-Information "Updated $Name to $Image on http://localhost:$Port"
+  # oder schlicht:
+  # Write-Output "Updated $Name to $Image on http://localhost:$Port"
